a489ba7ac81563db84504b9734acb0842e724618
bondlegends <botsetup7@gmail.com>
2024-11-06 15:50:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,29 +22,24 @@ let updateTimer;
 
 const music_list = [
     {
-        img : 'images/stay.png',
+        img : 'images/images/bg.jpg',
         name : 'Stay',
-        artist : 'The Kid LAROI, Justin Bieber',
-        music : 'music/stay.mp3'
+        artist : 'En Mannavva',
+        music : 'music/1.mp3'
     },
-    {
-        img : 'images/fallingdown.jpg',
-        name : 'Falling Down',
-        artist : 'Wid Cards',
-        music : 'music/fallingdown.mp3'
+      {
+        img : 'images/images/bg.jpg',
+        name : 'Stay',
+        artist : 'En Mannavva',
+        music : 'music/1.mp3'
     },
-    {
-        img : 'images/faded.png',
-        name : 'Faded',
-        artist : 'Alan Walker',
-        music : 'music/Faded.mp3'
+      {
+        img : 'images/images/bg.jpg',
+        name : 'Stay',
+        artist : 'En Mannavva',
+        music : 'music/1.mp3'
     },
-    {
-        img : 'images/ratherbe.jpg',
-        name : 'Rather Be',
-        artist : 'Clean Bandit',
-        music : 'music/Rather Be.mp3'
-    }
+ 
 ];
 
 loadTrack(track_index);
